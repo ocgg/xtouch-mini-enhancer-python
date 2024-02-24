03b3263b2f122a584e136cac7273990ac22ee77d
Olivier G <zotri@hotmail.com>
2024-02-24 20:52:54 +0100

    leds init

--- rtmidi_lib.py
+++ rtmidi_lib.py
@@ -1,11 +1,14 @@
 import rtmidi
 import time
 
-midiin = rtmidi.MidiIn()
+import lib.ctl.leds as leds
+
+fromxtouch = rtmidi.MidiIn()
+toxtouch = rtmidi.MidiOut()
 midiout = rtmidi.MidiOut()
 
 
-def print_message(message, prefix):
+def generate_message(message, prefix):
     if len(message) > 0:
         # Extraction du numéro de canal et du type de message
         status = message[0]
@@ -27,7 +30,7 @@ def print_message(message, prefix):
             midi_message = f"{prefix} N{note_status} - Channel: {channel}, Note: {note_number}, Velocity: {velocity}"
             return f"{midi_message:<50}"
 
-        # Autres types de messages non spécifiés
+        # Other messages
         else:
             return f"Message non spécifié: {message}"
     else:
@@ -44,48 +47,61 @@ def process_message(message):
     return message
 
 
-available_ports = midiin.get_ports()
+midi_in_ports = fromxtouch.get_ports()
 
-if available_ports:
+if midi_in_ports:
     # Connexion automatique au X-TOUCH MINI
-    xtouchmini_port = next((index for index, port_name in enumerate(available_ports) if "X-TOUCH MINI" in port_name), None)
+    xtouchmini_port = next((index for index, port_name in enumerate(midi_in_ports) if "X-TOUCH MINI" in port_name), None)
     print(f"X-Touch Mini trouvé sur le port MIDI: {xtouchmini_port}.")
-    midiin.open_port(xtouchmini_port)
-    print(f"Ouverture du port MIDI: {available_ports[xtouchmini_port]}")
+    fromxtouch.open_port(xtouchmini_port)
+    print(f"Ouverture du port MIDI: {midi_in_ports[xtouchmini_port]}")
 else:
     print("Aucun port MIDI disponible.")
     exit()
 
-available_output_ports = midiout.get_ports()
-if available_output_ports:
-    midiout.open_port(0)  # Ouvrir le premier port disponible
-# else:
-#     # Ouverture d'un port virtuel si aucun port physique n'est disponible
-#     midiout.open_virtual_port("My virtual output")
+midi_out_ports = toxtouch.get_ports()
+if midi_out_ports:
+    # Connexion automatique au X-TOUCH MINI
+    xtouchmini_port = next((index for index, port_name in enumerate(midi_out_ports) if "X-TOUCH MINI" in port_name), None)
+    toxtouch.open_port(xtouchmini_port)
+else:
+    print("Aucun X-Touch Mini trouvé.")
+    exit()
 
+# Ouverture d'un port virtuel
+midiout.open_virtual_port("OCGG")
+
+
+def led_on(toxtouch):
+    note = 0
+    velocity = 1
+    status = 0x90   # 0x90 = Note ON, add "+ 10" for canal 10
+    note_on = [status, note, velocity]
+    toxtouch.send_message(note_on)
+
+
+led_on(toxtouch)
 
 print("\nEn attente de signaux...\n")
 try:
     while True:
-        midiin_tuple = midiin.get_message()
-        if midiin_tuple is not None:
-            print("\033[2A\033[K", end="")
-            midi_in, time_stamp = midiin_tuple
-            print(print_message(midi_in, 'IN: '))
-            # display = print_message(midi_in, "IN: ")
+        fromxtouch_tuple = fromxtouch.get_message()
+        if fromxtouch_tuple is not None:
+            print("\033[2A\033[K", end="")  # Erase 2 lines above, each loop
+            midi_in, time_stamp = fromxtouch_tuple
+            print(generate_message(midi_in, 'IN: '))
             midi_out = process_message(midi_in)
-            # display += "        " + print_message(midi_out, "OUT:")
             midiout.send_message(midi_out)
-            # print("\033[F\033[K", end="")
-            print(print_message(midi_out, 'OUT:'))
-            # print(f"\r{display}", end="")
+            print(generate_message(midi_out, 'OUT:'))
         time.sleep(0.002)
 
 except KeyboardInterrupt:
     print("\nArrêt du programme.")
 
 finally:
-    midiin.close_port()
+    fromxtouch.close_port()
+    toxtouch.close_port()
     midiout.close_port()
-    del midiin
+    del fromxtouch
+    del toxtouch
     del midiout
