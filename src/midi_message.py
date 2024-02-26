0846233266f9e6ef7eea45427c58263ee1a7d41b
Olivier G <zotri@hotmail.com>
2024-02-26 16:31:57 +0100

    translate cc & cc notes, stable

--- src/midi_message.py
+++ src/midi_message.py
@@ -2,9 +2,16 @@ from copy import deepcopy
 
 
 class MidiMessage:
-    PRESET_CHANGE_RANGE = range(8, 16)   # 16 is excluded
-    LAYER_B_CC_RANGE = range(10, 19)     # 19 is excluded
-    LAYER_B_NOTES_RANGE = range(24, 48)  # 48 is excluded
+    PRESET_CHANGE_RANGE = range(8, 16)   # last range arg is excluded
+
+    LAYER_A_CC_RANGE = range(1, 9)
+    LAYER_B_CC_RANGE = range(10, 19)
+    LAYER_A_NOTE_RANGE = range(0, 24)
+    LAYER_B_NOTE_RANGE = range(24, 48)
+    # cc note : note message from a click on cc knob
+    LAYER_A_CC_NOTE_RANGE = range(0, 8)
+    # unused but left there as a reminder
+    # LAYER_B_CC_NOTE_RANGE = range(24, 32)
 
     def __init__(self, raw_msg, source):
         # TODO:better way to handle MidiMessage:first_byte
@@ -25,25 +32,28 @@ class MidiMessage:
     def is_cc(self):
         return self.msg_type == "CC"
 
+    def is_layer_A_cc_note(self):
+        return self.is_note() and self.knob in self.LAYER_A_CC_NOTE_RANGE
+
+    def is_layer_A_cc(self):
+        return self.is_cc() and self.knob in self.LAYER_A_CC_RANGE
+
     def is_preset_change(self):
         return self.is_note() and self.knob in self.PRESET_CHANGE_RANGE
 
-    def is_from_fader(self):
-        return self.is_cc() and self.knob in (9, 10)
-
-    def is_from_layer_b(self):
+    def is_from_layer_B(self):
         if self.is_cc():
             return self.knob in self.LAYER_B_CC_RANGE
         elif self.is_note():
-            return self.knob in self.LAYER_B_NOTES_RANGE
+            return self.knob in self.LAYER_B_NOTE_RANGE
 
     def translate(self, preset):
         new_msg = deepcopy(self)
         new_msg.source = "virtualout"
         if new_msg.is_cc():
             new_msg.knob += preset['cc_offset']
-        # elif new_msg.is_note():
-        #     new_msg.knob += preset['note_offset']
+        elif new_msg.is_note():
+            new_msg.knob += preset['note_offset']
         return new_msg
 
     def to_raw(self):
--- src/xtouch.py
+++ src/xtouch.py
@@ -4,15 +4,17 @@ import src.midi_message as midi_message
 
 # This is the main app controller
 class XTouch:
+    # NOTE : As the notes 8-15 are kept for preset change,
+    # they are unused for the virtual output
     PRESETS = [
-        {'cc_offset': 00},   # 'note_offset': 0},
-        {'cc_offset': 18},  # 'note_offset': 48},
-        {'cc_offset': 26},  # 'note_offset': 56},
-        {'cc_offset': 34},  # 'note_offset': 64},
-        {'cc_offset': 42},  # 'note_offset': 72},
-        {'cc_offset': 50},  # 'note_offset': 80},
-        {'cc_offset': 58},  # 'note_offset': 88},
-        {'cc_offset': 66}   # 'note_offset': 96}
+        {'cc_offset': 00,   'note_offset': 00},
+        {'cc_offset': 18,   'note_offset': 48},
+        {'cc_offset': 26,   'note_offset': 56},
+        {'cc_offset': 34,   'note_offset': 64},
+        {'cc_offset': 42,   'note_offset': 72},
+        {'cc_offset': 50,   'note_offset': 80},
+        {'cc_offset': 58,   'note_offset': 88},
+        {'cc_offset': 66,   'note_offset': 96}
     ]
 
     def __init__(self, midiin, midiout, virtualout, cli):
@@ -38,19 +40,23 @@ class XTouch:
         # Send to CLI for display
         self.cli.midiin_msg = msg
 
-        # if preset change (on NOFF 8-15), no virtual output
-        if msg.is_preset_change():
+        # Translate ONLY CC & CCnote from layer A
+        if msg.is_layer_A_cc() or msg.is_layer_A_cc_note():
+            translated_msg = msg.translate(self.current_preset)
+            self._display_and_send(translated_msg)
+        # no virtual output if msg is preset change (notes 8-15)
+        elif msg.is_preset_change():
+            # change preset on noteoff (else do nothing)
             if msg.msg_type == "NOTEOFF":
                 self.current_preset = self.PRESETS[msg.knob - 8]
-        # TODO: Do not translate fader or layerB messages
-        elif msg.is_from_fader() or msg.is_from_layer_b():
-            # translate without offset (juste change msg.source)
-            translated_msg = msg.translate(self.PRESETS[0])
-            self.cli.virtualout_msg = translated_msg
-            self.virtualout.send_message(msg.to_raw())
+        # Do not translate any other message
         else:
-            translated_msg = msg.translate(self.current_preset)
-            self.cli.virtualout_msg = translated_msg
-            self.virtualout.send_message(translated_msg.to_raw())
+            # this only change msg.source (does not apply offset)
+            translated_msg = msg.translate(self.PRESETS[0])
+            self._display_and_send(translated_msg)
 
     # PRIVATE METHODS #
+
+    def _display_and_send(self, msg):
+        self.cli.virtualout_msg = msg
+        self.virtualout.send_message(msg.to_raw())
